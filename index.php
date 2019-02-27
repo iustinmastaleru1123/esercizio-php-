1005d547303a5213e7b3173df4d0520c13fdd417
Iustin <45000008+iustinmastaleru1123@users.noreply.github.com>
2019-02-27 10:15:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+
+      <title>My Form!</title>
+  
+  </head>
+  <body>
+      <div class="container"> 
+      <h1> Insierisci i tuoi dati </h1> 
+       <form>
+       
+       <div class="form-group">
+    <label for="Name">nome</label>
+    <input type="text" class="form-control" id="name" aria-describedby="namelHelp" placeholder="Il tuo nome">
+    <small id="emailHelp" class="form-text text-muted">Inserisci il tuo nome</small>
+  </div>
+  <div class="form-group">
+    <label for="Name">cognome</label>
+    <input type="text" class="form-control" id="name" aria-describedby="namelHelp" placeholder="Il tuo cognome">
+    <small id="emailHelp" class="form-text text-muted">Inserisci il tuo cognome</small>
+  </div>
+  </form>
+    </body>
+<script src="js/jquery-3.3.1.slim.min.js"> </script>
+    <script src="js/1.14.3/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </html>
